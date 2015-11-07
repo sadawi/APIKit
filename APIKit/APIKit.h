c3897482edb24011dcba254f84c246b24641892b
Sam Williams <samuel.williams@gmail.com>
2015-11-07 13:54:36 -0500

    Initial Commit

---
+++ APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by Sam Williams on 11/7/15.
+//  Copyright © 2015 Sam Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
