28e46ff2ec8c4406a06989328761de2493c5a928
Sam Williams <samuel.williams@gmail.com>
2015-11-08 00:04:17 -0500

    more promise testing

--- APIKitTests/APIKitTests.swift
+++ APIKitTests/APIKitTests.swift
@@ -37,7 +37,7 @@ class APIKitTests: XCTestCase {
         super.setUp()
         
         let store = MemoryDataStore()
-        store << Person(name: "Bob", age:44)
+        store << Person(name: "Bob", age: 44)
         store << Person(name: "Alice", age: 55)
 
         ModelManager.sharedInstance.dataStore = store
@@ -50,18 +50,34 @@ class APIKitTests: XCTestCase {
     
     func testResources() {
         let expectation = expectationWithDescription("Waiting")
-        ModelManager.sharedInstance.dataStore.list(Person).then { results -> Promise<Person?> in
+        ModelManager.sharedInstance.dataStore.list(Person).then { results -> () in
             XCTAssertEqual(2, results.count)
-            let person = results[0]
-            return ModelManager.sharedInstance.dataStore.lookup(person.dynamicType, identifier: person.identifier!)
-            }.then { person -> () in
-                XCTAssertNotNil(person)
-                XCTAssertEqual("Bob", person?.name)
-                expectation.fulfill()
+            expectation.fulfill()
         }
         self.waitForExpectationsWithTimeout(1, handler:nil)
-
     }
+    
+    func testResource() {
+        let didSave = expectationWithDescription("save")
+        let didLookup = expectationWithDescription("lookup")
+        
+        let a = Person(name: "Kevin", age: 33)
+        XCTAssertNil(a.identifier)
+        
+        ModelManager.sharedInstance.dataStore.save(a).then { model -> Promise<Person?> in
+            XCTAssertNotNil(model.identifier)
+            didSave.fulfill()
+            let id = model.identifier!
+            return ModelManager.sharedInstance.dataStore.lookup(a.dynamicType, identifier: id)
+            }.then { model -> () in
+                XCTAssertNotNil(model)
+                XCTAssert(model! === a)
+                didLookup.fulfill()
+        }
+        
+        self.waitForExpectationsWithTimeout(1, handler:nil)
+    }
+
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
