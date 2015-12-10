8f73acbd8c5a240706ecf3ee3ac514d82a67a16d
Sam Williams <samuel.williams@gmail.com>
2015-12-10 17:07:04 -0500

    fixed missing key

--- APIKit/ModelField.swift
+++ APIKit/ModelField.swift
@@ -11,7 +11,18 @@ import MagneticFields
 
 public class ModelField<T: Model>: Field<T> {
     public override init(value:T?=nil, name:String?=nil, priority:Int=0, key:String?=nil) {
-        super.init(value: value, name: name, priority: priority)
+        super.init(value: value, name: name, priority: priority, key: key)
+    }
+    
+    public override func defaultValueTransformer() -> ValueTransformer<T> {
+        return ModelValueTransformer<T>()
+    }
+}
+
+// Not sure about the right hierarchy here.
+public class ModelArrayField<T: Model>: ArrayField<T> {
+    public override init(value:[T]?=nil, name:String?=nil, priority:Int=0, key:String?=nil) {
+        super.init(value: value, name: name, priority: priority, key: key)
     }
     
     public override func defaultValueTransformer() -> ValueTransformer<T> {
