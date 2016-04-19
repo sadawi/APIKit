b0aca303186c4154b9d03c24ef95ccb1147c8c94
Sam Williams <samuel.williams@gmail.com>
2016-04-19 12:12:29 -0400

    can't override extension methods, so moving validation back into Model.swift

--- APIKit/Model.swift
+++ APIKit/Model.swift
@@ -443,4 +443,39 @@ public class Model: NSObject, Routable, NSCopying {
         }
         return results
     }
+    
+    // MARK: Validation
+    
+    public func addError(keyPath path:String, message:String) {
+        if let field = self.fieldForKeyPath(path) {
+            field.addValidationError(message)
+        }
+    }
+    
+    /**
+     Test whether this model passes validation.  All fields will have their validation states updated.
+     
+     By default, related models are not themselves validated.  Use the `requireValid()` method on those fields for deeper validation.
+     */
+    public func validate() -> ValidationState {
+        self.resetValidationState()
+        var messages: [String] = []
+        
+        self.visitAllFields(recursive: false) { field in
+            if case .Invalid(let fieldMessages) = field.validate() {
+                messages.appendContentsOf(fieldMessages)
+            }
+        }
+        
+        if messages.count == 0 {
+            return .Valid
+        } else {
+            return .Invalid(messages)
+        }
+    }
+    
+    public func resetValidationState() {
+        self.visitAllFields { $0.resetValidationState() }
+    }
+
 }
