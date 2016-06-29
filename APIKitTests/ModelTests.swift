3a298932a5c04e78c53942db88c85f56856c047a
Sam Williams <samuel.williams@gmail.com>
2016-06-29 13:25:35 -0400

    renamed test file

--- APIKitTests/ModelTests.swift
+++ APIKitTests/ModelTests.swift
@@ -10,7 +10,7 @@ import XCTest
 import MagneticFields
 import PromiseKit
 
-class FieldModelTests: XCTestCase {
+class ModelTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
